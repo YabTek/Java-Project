6a9156d71d51a23f07ea50a3709d30676df98b6b
YabTek <yabtekuam@gmail.com>
2022-11-27 13:41:26 +0300

    netflix

--- frontend/src/screens/HomePage/HomePage.js
+++ frontend/src/screens/HomePage/HomePage.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './HomePage.css'
 import Header from '../../components/Header/Header'
 
+
 const HomePage = () => {
   return (
     <div className = "home-main">
       <Header/>
+      <img src = {require('../../images/bg.png')} alt = "" width = "100%"/>
     </div>
   )
 }
