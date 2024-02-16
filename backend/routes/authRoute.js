f8804f1ba61b555ca59f8c64e779be558b409da3
YabTek <yabtekuam@gmail.comremote: Invalid username or password>
2024-02-16 14:40:58 +0300

    java project

--- backend/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {registerUser,logUser } = require('../controllers/authController')
-
-router.route('/register').post(registerUser)
-router.route('/login').post(logUser)
-
-module.exports = router
